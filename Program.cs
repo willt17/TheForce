8e9c3218bf055e4a410eba12e2687e1dc9de6ca7
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-03-10 11:03:16 -0600

    added credits bet system

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace theForce
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to blasters!");
+            int credits = 100;
+            Blasters(ref credits);
+        }
+        static void Blasters(ref int credits)
+        {
+            int currBet = -1;
+            while (currBet == -1)
+            {
+                currBet = betSize(credits);
+            }
+            System.Console.WriteLine(currBet);
+        }
+        static int betSize(int credits)
+        {
+            int temp = 0;
+            System.Console.WriteLine("How much would you like to bet?");
+            temp = int.Parse(Console.ReadLine());
+            if (temp <= credits)
+            {
+                return temp;
+            }
+            else
+            {
+                System.Console.WriteLine("You don't have enough credits for that bet.");
+                temp = -1;
+                return temp;
+            }
+
         }
     }
 }
