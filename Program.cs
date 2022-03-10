baa5fb60159fbf9203a63b15a63ae8078003e98b
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-03-10 11:56:42 -0600

    added card deck method

--- Program.cs
+++ Program.cs
@@ -12,12 +12,13 @@ namespace theForce
         }
         static void Blasters(ref int credits)
         {
+            BlastersExplanation();
             int currBet = -1;
             while (currBet == -1)
             {
                 currBet = betSize(credits);
             }
-            System.Console.WriteLine(currBet);
+            // System.Console.WriteLine(currBet);
         }
         static int betSize(int credits)
         {
@@ -35,6 +36,16 @@ namespace theForce
                 return temp;
             }
 
+        }
+        static void BlastersExplanation()
+        {
+            System.Console.WriteLine("Welcome to the blasters game.");
+            System.Console.WriteLine("You will be given a stack of 10 cards and shown the value of the first one.");
+            System.Console.WriteLine("You must then guess if the next card will be higher or lower in value.");
+            System.Console.WriteLine("If you get less than 5 cards correct you lose your bet.");
+            System.Console.WriteLine("If ypu get between 5 and 7 cards correct you get your credits back.");
+            System.Console.WriteLine("If you get between 7 and 10 cards correct you double your bet.");
+            System.Console.WriteLine("If you get all 10 correct you triple your bet.");
         }
     }
 }
