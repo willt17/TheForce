760bf974d66e1b075a2ac37d95157d055a1690ad
Will Tidwell <jwtidwell1@crimson.ua.edu>
2022-03-10 21:52:06 -0600

    finished the card deck method

--- Program.cs
+++ Program.cs
@@ -12,13 +12,16 @@ namespace theForce
         }
         static void Blasters(ref int credits)
         {
+            string[] deckOfCards = new string[12];
+            DeckGenerator(deckOfCards);
             BlastersExplanation();
+            // CardPrint(deckOfCards);
             int currBet = -1;
             while (currBet == -1)
             {
                 currBet = betSize(credits);
             }
-            // System.Console.WriteLine(currBet);
+            System.Console.WriteLine(currBet);
         }
         static int betSize(int credits)
         {
@@ -46,6 +49,32 @@ namespace theForce
             System.Console.WriteLine("If ypu get between 5 and 7 cards correct you get your credits back.");
             System.Console.WriteLine("If you get between 7 and 10 cards correct you double your bet.");
             System.Console.WriteLine("If you get all 10 correct you triple your bet.");
+            System.Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+            System.Console.WriteLine("\n");
         }
+        static void DeckGenerator(string[] deckOfCards)
+        {
+            deckOfCards[0] = "Ace";
+            deckOfCards[1] = "Two";
+            deckOfCards[2] = "Three";
+            deckOfCards[3] = "Four";
+            deckOfCards[4] = "Five";
+            deckOfCards[5] = "Six";
+            deckOfCards[6] = "Seven";
+            deckOfCards[7] = "Eight";
+            deckOfCards[8] = "Nine";
+            deckOfCards[9] = "Joker";
+            deckOfCards[10] = "Queen";
+            deckOfCards[11] = "King";
+
+        }
+        // static void CardPrint(string[] deckOfCards)
+        // {
+        //     for (int i = 0; i < deckOfCards.Length; i++)
+        //     {
+        //         System.Console.WriteLine(deckOfCards[i]);
+        //     }
+        // }
     }
 }
